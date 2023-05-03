c347339326e4b8b43ae4ad6a5105a38d2979b0f6
Tristan Bierig <trista.bierig@gmail.com>
2023-05-03 18:13:10 +0200

    directory change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentPokemon;
-let pokemonNumber = 10;
+let pokemonNumber = 100;
 
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/' + pokemonNumber;
@@ -13,27 +13,56 @@ async function loadPokemon() {
     let responseSpecies = await fetch(currentPokemon.species.url);
     currentSpecies = await responseSpecies.json();
 
-
-
     console.log('Loaded pokemon', currentPokemon, currentSpecies, /*currentAbilities*/);
     renderPokemonInfo();
+    renderListOfPokemon();
 }
 
 
 function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentSpecies.names[5].name;
-    document.getElementById('pokemonId').innerHTML += pokemonNumber.toString().padStart(3, "0");
-    document.getElementById('pokemonSprite').src = currentPokemon.sprites.other["official-artwork"].front_default;
-    document.getElementById('pokedexOverview').style.backgroundColor = currentSpecies.color.name;
-    document.getElementById('pokedexStats').style.boxShadow = `0 -24px 0 0 ${currentSpecies.color.name}`;
+    document.getElementById('pokedexName').innerHTML = currentSpecies.names[5].name; // Name of Pokemon
+    document.getElementById('pokedexId').innerHTML += pokemonNumber.toString().padStart(3, "0"); // ID of Pokemon
+    document.getElementById('pokedexSprite').src = currentPokemon.sprites.other["official-artwork"].front_default; // Image of Pokemon
+    document.getElementById('pokedexOverview').style.backgroundColor = currentSpecies.color.name; // Color of Pokemon used as BG
+    document.getElementById('pokedexStats').style.boxShadow = `0 -24px 0 0 ${currentSpecies.color.name}`; // Color of Pokemon used for Styling of Pokedex Card
     
     for (let i = 0; i < currentPokemon.types.length; i++) {
         let type = currentPokemon.types[i].type.name;
-        document.getElementById('pokemonTypes').innerHTML +=`
+        document.getElementById('pokedexTypes').innerHTML +=`
         <div class="${type} type">
             ${type}
         </div>
     `;
     }
-    
+}
+
+
+function renderListOfPokemon() {
+    let listContainer = document.getElementById('listOfPokemon');
+    let pokemonName = currentSpecies.names[5].name; // Name of Pokemon
+    let pokemonID = pokemonNumber.toString().padStart(3, "0"); // ID of Pokemon
+    let pokemonImage = currentPokemon.sprites.other["official-artwork"].front_default; // URL of Image of Pokemon
+    let pokemonColor = currentSpecies.color.name; // Color of Pokemon used as BG
+
+    listContainer.innerHTML = '';
+
+ for (let i = 1; i < 10; i++) {
+    listContainer.innerHTML += singlePokemonCardHTML(i, pokemonName, pokemonID, pokemonImage, pokemonColor);
+ }
+}
+
+
+function singlePokemonCardHTML(i, pokemonName, pokemonID, pokemonImage, pokemonColor) {
+    return `
+    <div class="pokemon-cards ${pokemonColor}" id="pokemon${i}">
+        <div id="">
+            <span class="pokemonName" id="">${pokemonName}</span>
+            <span class="pokemonId" id="pokemonId">#${pokemonID}</span>
+            <div class="pokemonTypes" id="pokemonTypes"></div>
+        </div>
+
+        <img class="pokeball-bg" src="/src/img/pokeball-bg.png" alt="">
+        <img id="pokemonSprite" src="${pokemonImage}" alt="">
+    </div>
+    `;
 }
